89ae69a5b9da2b368f1c7b9046e9b6b95df3662a
Mehmet Emre Yildiz <mehmeyil@c4r2p9.42vienna.com>
2024-08-23 23:03:09 +0200

    up

--- Cub_4.0/read_check_colors.c
+++ Cub_4.0/read_check_colors.c
@@ -3,18 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   read_check_colors.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtrojano <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mehmeyil <mehmeyil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 07:22:03 by mtrojano          #+#    #+#             */
-/*   Updated: 2024/08/23 18:46:38 by mtrojano         ###   ########.fr       */
+/*   Updated: 2024/08/23 23:02:46 by mehmeyil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-//need to check -> len of arguments[0]; more than 1 -> error; (example: "F," <- wrong same for "NO,") 
+//need to check -> len of arguments[0]; more than 1 -> error (THIS IS DONE AND TESTED, LINE 105 and 121 one new function)!! 
+//; (example: "F," <- wrong same for "NO,") 
 //and then enough if check d->colors_f and d->colors_c if there are only 2 commas and not after each other.
-//or in assign_fc() -> then len of name and commas in all_values.
+//or in assign_fc() -> then len of name and commas in all_values. (I HAVE TO LEAVE NOW GONNA DO IT TOMORROW)
 
 void	print_arr(char **arr)
 {
@@ -102,10 +103,26 @@ int	check_for_ceiling(t_data *d, char **rgb)
 	return (0);
 }
 
+int	check_numbers2(t_data *d, char **arguments, char **rgb)
+{
+	if (ft_strncmp(arguments[0], "C", 1) == 0)
+	{
+		if (ft_strlen(arguments[0]) > 1)
+			return (-1);
+		rgb = ft_split(d->color_c, ',');
+		if (!rgb)
+			return (ft_error("Error: malloc failed\n"));
+		if (check_for_ceiling(d, rgb) == -1)
+			return (free_env(&rgb), -1);
+	}
+	return (0);
+}
+
 int	check_numbers(t_data *d, char **arguments, int place)
 {
 	char	**rgb;
 
+	rgb = NULL;
 	if (place == 0)
 	{
 		if (join_assign_fc_colors(d, arguments) == -1)
@@ -113,22 +130,16 @@ int	check_numbers(t_data *d, char **arguments, int place)
 	}
 	if (ft_strncmp(arguments[0], "F", 1) == 0)
 	{
+		if (ft_strlen(arguments[0]) > 1)
+			return (ft_error("Error ; Arguments are not correct\n"));
 		rgb = ft_split(d->color_f, ',');
 		if (!rgb)
 			return (ft_error("Error: malloc failed\n"));
-		// if (check_rgb_is_valid(rgb) == -1)
-		// 	printf("Say something\n");
 		if (check_for_floor(d, rgb) == -1)
 			return (free_env(&rgb), -1);
 	}
-	if (ft_strncmp(arguments[0], "C", 1) == 0)
-	{
-		rgb = ft_split(d->color_c, ',');
-		if (!rgb)
-			return (ft_error("Error: malloc failed\n"));
-		if (check_for_ceiling(d, rgb) == -1)
-			return (free_env(&rgb), -1);
-	}
+	if (check_numbers2(d, arguments, rgb) == -1)
+		return(ft_error("Error ; Arguments are not correct\n"));
 	free_env(&rgb);
 	return (0);
 }
