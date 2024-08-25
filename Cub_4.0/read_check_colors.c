079bf878b2d12587696f2d547972520f3e88bbc6
Michal Trojanowski <mtrojano@c4r2p8.42vienna.com>
2024-08-25 23:45:16 +0200

    msg in comments

--- Cub_4.0/read_check_colors.c
+++ Cub_4.0/read_check_colors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read_check_colors.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mehmeyil <mehmeyil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mtrojano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 07:22:03 by mtrojano          #+#    #+#             */
-/*   Updated: 2024/08/24 14:02:56 by mehmeyil         ###   ########.fr       */
+/*   Updated: 2024/08/25 23:43:49 by mtrojano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 //; (example: "F," <- wrong same for "NO,") (THIS IS DONE AND TESTED => I've added checks for Directions (WE,NO,EA,SO in read_check_tex_path.c))
 //and then enough if check d->colors_f and d->colors_c if there are only 2 commas and not after each other.
 //or in assign_fc() -> then len of name and commas in all_values. (THIS IS DONE AND TESTED => IN COMMA CHECK HOWEVER I GOT QUESTIONS FOR YOU)
+
+//check_format is something what I had in mind, I'll explain when we see each other :) I commented out your function for now (for testing) but basically it's just a matter of how we decide to handle the format
 
 void	print_arr(char **arr)
 {
@@ -27,6 +29,34 @@ void	print_arr(char **arr)
 		i++;
 	}
 }
+
+int	check_format(char *all_values, int i, int comma, int format)
+{
+	while (all_values[i] != '\n')
+	{
+		if (all_values[i] != ' ' && all_values[i] != ','
+			&& ft_isdigit(all_values[i]) != 2048)
+			return (ft_error("Invalid format for color arguments\n"));
+		if (all_values[i] == ',')
+			comma++;
+		i++;
+	}
+	if (comma != 2)
+		return (ft_error("Invalid format for color arguments\n"));
+	i = 0;
+	while (all_values[i] != '\n')
+	{
+		if (all_values[i] == ',')
+			format++;
+		if (ft_isdigit(all_values[i]) == 2048)
+			format -= 1;
+		if (format == 2)
+			return (ft_error("Invalid format for color arguments\n"));
+		i++;
+	}
+	return (0);
+}
+
 int	check_for_commas(char *str)
 {
 	if (!str)
@@ -43,13 +73,15 @@ int	check_for_commas(char *str)
 }
 int	assign_fc(t_data *d, char *name, char *all_values)
 {
+	if (check_format(all_values, 0, 0, 0) == -1)
+		return (-1);
 	if (ft_strncmp(name, "F", 1) == 0)
 	{
 		if (d->color_f)
 			return (ft_error("Cannot have more than one F\n"));
 		d->color_f = ft_strdup(all_values);
-		if (check_for_commas(d->color_f) == -1)
-			return (ft_error("Cannot have empty value\n"));
+		// if (check_for_commas(d->color_f) == -1)
+		// 	return (ft_error("Cannot have empty value\n"));
 		if (!d->color_f)
 			return (ft_error("Error: malloc failed\n"));
 	}
@@ -58,8 +90,8 @@ int	assign_fc(t_data *d, char *name, char *all_values)
 		if (d->color_c)
 			return (ft_error("Cannot have more than one C\n"));
 		d->color_c = ft_strdup(all_values);
-		if (check_for_commas(d->color_f) == -1)
-			return (ft_error("Cannot have empty value\n"));
+		// if (check_for_commas(d->color_c) == -1)
+		// 	return (ft_error("Cannot have empty value\n"));
 		if (!d->color_c)
 			return (ft_error("Error: malloc failed\n"));
 	}
@@ -126,6 +158,8 @@ int	check_numbers2(t_data *d, char **arguments, char **rgb)
 	{
 		if (ft_strlen(arguments[0]) > 1)
 			return (-1);
+		if (check_format(d->color_c, 0, 0, 0) == -1)
+			return (-1);
 		rgb = ft_split(d->color_c, ',');
 		if (!rgb)
 			return (ft_error("Error: malloc failed\n"));
@@ -149,6 +183,8 @@ int	check_numbers(t_data *d, char **arguments, int place)
 	{
 		if (ft_strlen(arguments[0]) > 1)
 			return (ft_error("Error ; Arguments are not correct\n"));
+		if (check_format(d->color_f, 0, 0, 0) == -1)
+			return (-1);
 		rgb = ft_split(d->color_f, ',');
 		if (!rgb)
 			return (ft_error("Error: malloc failed\n"));
@@ -156,7 +192,7 @@ int	check_numbers(t_data *d, char **arguments, int place)
 			return (free_env(&rgb), -1);
 	}
 	if (check_numbers2(d, arguments, rgb) == -1)
-		return(ft_error("Error ; Arguments are not correct\n"));
+		return(-1);
 	free_env(&rgb);
 	return (0);
 }
